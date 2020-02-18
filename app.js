fb1ab3a1f1fd9e7abc356cd8285efcc9fa0a2ae1
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-18 18:15:33 +0100

    fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect(`${dbUrl}`, {useNewUrlParser: true})
+  .connect(`${process.env.DBURL}`, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
