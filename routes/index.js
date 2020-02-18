ee14660c1bc150fc78be3b0d66289f6097cae581
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-18 18:16:47 +0100

    ...

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,6 @@ router.get('/', (req, res, next) => {
 });
 
 
-<<<<<<< HEAD
-
-=======
 router.get('/movies', (req, res, next) => {
 
   axios.get('https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.gte=2020-02-15&year=2020')
@@ -19,7 +16,6 @@ router.get('/movies', (req, res, next) => {
     })
     
     
->>>>>>> 4c81e2f98f20c3a506e58f444335eb9b2258d6b7
 
 // router.get('/movies', (req,res,next) =>{
 
@@ -32,14 +28,5 @@ router.get('/movies', (req, res, next) => {
 
 // })
 
-<<<<<<< HEAD
-function generateUrl(path){
-  const url = `https://api.themoviedb.org/3${path}api_key=c9f84c134bb1d07c82ecf21fbb8de863`
 
-  return url;
-}
-=======
-
-
->>>>>>> 4c81e2f98f20c3a506e58f444335eb9b2258d6b7
 module.exports = router;
