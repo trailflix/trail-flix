4d43eb20a3404a4308bf8d3b506a0d8088ec599c
armetegithub <armomomete@protonmail.com>
2020-02-21 13:45:05 +0100

    search bar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,49 +1,38 @@
-window.onload = function () {
-
-  let addFavBtn = document.querySelector('#button')
-  let deleteFavBtn = document.querySelectorAll('.btn-delete')
+window.onload = function() {
+  let addFavBtn = document.querySelector("#button");
+  let deleteFavBtn = document.querySelectorAll(".btn-delete");
 
   if (addFavBtn) {
-    addFavBtn.onclick = function () {
-      let hiddenId = document.querySelector("#hiddenId").value
-      let hiddenName = document.querySelector("#hiddenName").value
-      let hiddenImage = document.querySelector("#hiddenImage").value
+    addFavBtn.onclick = function() {
+      let hiddenId = document.querySelector("#hiddenId").value;
+      let hiddenName = document.querySelector("#hiddenName").value;
+      let hiddenImage = document.querySelector("#hiddenImage").value;
       let favMovie = {
         id: hiddenId,
         title: hiddenName,
         img: hiddenImage
-      }
-      axios.post('/movies/addMovie', favMovie)
-        .then(() => console.log(favMovie))
-    }
+      };
+      axios
+        .post("/movies/addMovie", favMovie)
+        .then(() => console.log(favMovie));
+    };
+  }
+
+  if (deleteFavBtn) {
+    deleteFavBtn.forEach(button => {
+      button.addEventListener("click", function() {
+        let idMovie = document.querySelector(".movie-id").value;
+        console.log(idMovie);
+        deleteMovie(idMovie);
+      });
+    });
   }
 
 
-  if(deleteFavBtn){
-    deleteFavBtn.forEach(button =>{
-      
-      button.addEventListener('click',function(){
-        let idMovie = document.querySelector('.movie-id').value
-        console.log(idMovie)
-        deleteMovie(idMovie)
-      })
-    })
-  }
 
-
-  function deleteMovie(id){
-    axios.post(`/delete/${id}`,{id})
-    .then(()=>console.log("ok"))
-  }
-
-
-/*   document.querySelector('#deletefav-{{id}}').onclick = function (e) {
-    e.preventDefault()
-    console.log('hola franky guapi') */
-    //axios.post("/movies/unfav/{{id}}").then(() => axios.get("/profile").then((favoritasAqui) => //repintado))
-  
 
 }
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,9 @@ function ensureAuthenticated(req, res, next) {
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/search', (req, res, next) => {
+  res.render('search');
+});
 
 router.get('/movie/:id', function (req, res) {
 
@@ -27,140 +30,48 @@ router.get('/movie/:id', function (req, res) {
       key: `${process.env.YT_API}`
     };
     let movie = response.data
-    //   youtubeSearch(`${movie.title} trailer`, opts, function(err, results) {
-    //  if(err) return console.log(err);
-    //   let video = results[0]
-    //   console.log({video,movie})
-      res.render("movie-detail", {/*video, */movie})
+    youtubeSearch(`${movie.title} trailer`, opts, function(err, results) {
+    if(err) return console.log(err);
+    let video = results[0]
+    console.log({video,movie})
+    res.render("movie-detail", {video, movie})
     
     
   })
 
 })
 
+})
+
+  router.post("/search", ensureAuthenticated, (req, res) => {
+    let film = req.body.query
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&language=en-US&query=" +
+          film +
+          "&page=1&include_adult=false"
+      )
+      .then(response => {
+        console.log(response)
+        let movies = response.data.results;
+        res.render("search", {movies});
+     //   console.log(jsondata);
+      });
+  });
 
 
+// router.post("/movies/addMovie", (req, res) => {
+//   console.log(req.body)
+//   let movie = req.body;
+//   User.findByIdAndUpdate(req.user._id, {$push: {favlist: movie}})
+//   .then(()=> res.json({ok:true}))
+//   .catch((err)=>res.json(err))
+// });
 
-
-router.post("/movies/addMovie", (req, res) => {
-  console.log(req.body)
-  let movie = req.body;
-  
-  User.findByIdAndUpdate(req.user._id, {$push: {favlist: movie}})
-  .then(()=> res.json({ok:true}))
-  .catch((err)=>res.json(err))
-
-  
-});
 
 module.exports = router
 
-router.post('/delete/:id', (req,res) =>{
-  console.log(req.body)
-  User.findByIdAndUpdate(req.user._id, {$pull: {favlist: {id: req.body.id}}})
-  .then(()=>{
-    res.redirect("/auth/profile")
-    .then(err => console.log(err))
-  })
-})
 
-/* router.delete("/auth/profile", (req, res) => {
-  console.log('hago algo')
-}) */
-
-
-// router.get("/categories", (req, res) => {
-
-//   res.render("categories");
-// });
-
-// function ensureAuthenticated(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     return next();
-//   } else {
-//     res.redirect('login')
-//   }
-// }
-
-
-// router.get("/categories", (req, res) => {
-
-//   res.render("categories");
-// });
-
-
-// router.get("/action", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=28&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesAction = response.data.results;
-//       res.render("action", { moviesAction });
-//     });
-// });
-
-// router.get("/drama", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=18&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesDrama = response.data.results;
-//       res.render("drama", { moviesDrama });
-//     });
-// });
-
-// router.get("/comedy", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=35&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesComedy = response.data.results;
-//       res.render("comedy", { moviesComedy });
-//     });
-// });
-
-// router.get("/terror", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=27&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesTerror = response.data.results;
-//       res.render("terror", { moviesTerror });
-//     });
-// });
-
-// router.get("/romance", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=10749&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesRomance = response.data.results;
-//       res.render("romance", { moviesRomance });
-//     });
-// });
-
-
-// router.get("/science_fiction", ensureAuthenticated, (req, res) => {
-//   axios
-//     .get(
-
-//       "https://api.themoviedb.org/3/discover/movie?api_key=c9f84c134bb1d07c82ecf21fbb8de863&with_genres=878&release_date.gte=2020-07-15&year=2020"
-//     )
-//     .then(response => {
-//       let moviesScienceFiction = response.data.results;
-//       res.render("science_fiction", { moviesScienceFiction });
-//     });
-// });
 
 
 
